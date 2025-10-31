7a897aef2ce401899383a40aa7fefbe1b5a24684
Francis <franciskarani15@gmail.com>
2025-10-31 20:23:29 +0300

    configures the db, creates the first flask instance

--- server/app.py
+++ server/app.py
@@ -1,8 +1,21 @@
-from flask import Flask 
+from flask import Flask
 from flask_restful import Resource, Api
 from flask_migrate import Migrate
+from model import db, Car, Dealer
 
-
+# creates the first flask instance of app
 app = Flask(__name__)
+
+# wraps the app with Flask-restfulls API class.
 api = Api(app)
+
+
+# configuring the database
+app.config['SQLALCHEMY-DATABASE-URI'] = 'sqlite:///auto.db'
+app.config['SQLALCHEMY-TRACK-MODIFICATIONS'] = False
+
+db.init_app()
+migrate=Migrate(db, app)
+
+
 
